6d61aaacf66493e59e6a79d694446e6f4093d05c
oz <oz.rnds@gmail.com>
2023-09-21 23:24:31 +0300

    test(scripts): Update the url invoked in the tasks

--- scripts/stress_tests.py
+++ scripts/stress_tests.py
@@ -18,5 +18,5 @@ from components.db_api import db_instance
 if __name__ == "__main__":
     # Create a loop that sends many tasks to be invoked inorder to check the service ability to handle them.
     for i in range(10000):
-        db_instance.insert_timer(f"http://google.com/", datetime.now())
+        db_instance.insert_timer(f"http://127.0.0.1/", datetime.now())
         time.sleep(0.01)
